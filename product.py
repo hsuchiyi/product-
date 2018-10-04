71094541224fac052aaaeee01b01d652365863af
hsuchiyi <hsuchiyi@gmail.com>
2018-10-05 01:48:02 +0800

    read file

--- product.py
+++ product.py
@@ -1,15 +1,24 @@
 products = []
+with open('product.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品, 價格' in line:
+            continue  #直接反覆的概念
+        name, price = line.strip().split(',')     #切成兩半後直接填入
+        products.append([name, price])
+print(products)
+
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
     price = input('請輸入商品價格: ')
+    int(price)
     products.append([name,price])
 print(products)
 for p in products:
    print(p[0], '的價格是', p[1])
 
 with open('product.csv', 'w', encoding='utf-8') as f:
-   f.write('價格,商品\n')
+   f.write('商品, 價格\n')
    for p in products:
-    f.write(p[0] + ',' + p[1] + '\n') 
+    f.write(p[0] + ',' + str(p[1]) + '\n') 
