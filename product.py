a936d686339d6dc4bb7ae6446a55539aa48eb1ef
hsuchiyi <hsuchiyi@gmail.com>
2018-10-04 21:47:19 +0800

    create cvs

--- product.py
+++ product.py
@@ -6,3 +6,9 @@ while True:
     price = input('請輸入商品價格: ')
     products.append([name,price])
 print(products)
+for p in products:
+   print(p[0], '的價格是', p[1])
+
+with open('product.csv', 'w') as f:
+   for p in products:
+    f.write(p[0] + ',' + p[1] + '\n') 
